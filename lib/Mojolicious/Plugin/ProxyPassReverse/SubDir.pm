215c735a7cd759dc241b0b3b4763c5f0befdc80e
s-murata <singo@dioib.com>
2015-10-06 21:36:22 +0900

    initial commit

---
+++ lib/Mojolicious/Plugin/ProxyPassReverse/SubDir.pm
@@ -0,0 +1,32 @@
+package Mojolicious::Plugin::ProxyPassReverse::SubDir;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Mojolicious::Plugin::ProxyPassReverse::SubDir -
+
+=head1 SYNOPSIS
+
+  use Mojolicious::Plugin::ProxyPassReverse::SubDir;
+
+=head1 DESCRIPTION
+
+Mojolicious::Plugin::ProxyPassReverse::SubDir is
+
+=head1 AUTHOR
+
+Shingo MURATA E<lt>murata@cpan.orgE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
